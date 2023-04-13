6549c7e3edd67546e00f5efd942951b3d1ccecf7
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-04-13 23:34:32 +0530

    create sidebar and statusbar

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='post__container'>
+<div className="post__card__container">
+    <div className="post__card">
+        
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Post
